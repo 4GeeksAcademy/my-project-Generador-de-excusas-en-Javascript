e9fb1e0ac0ce8eae38052759d2114ff08e300442
Flodsc <florenciac0812@gmail.com>
2024-09-23 22:59:37 +0000

    finished project

--- src/app.js
+++ src/app.js
@@ -9,3 +9,33 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+window.onload = function() {
+  // Partes de la excusa
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  // Función para generar las excusas
+  function getRandomIndex(arr) {
+    return Math.floor(Math.random() * arr.length);
+  }
+
+  // Generar una excusa aleatoria
+  let excuse =
+    who[getRandomIndex(who)] +
+    " " +
+    action[getRandomIndex(action)] +
+    " " +
+    what[getRandomIndex(what)] +
+    " " +
+    when[getRandomIndex(when)];
+
+  document.getElementById("excuse").innerHTML = excuse;
+};
